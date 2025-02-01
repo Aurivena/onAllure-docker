d87ba7c34b872d1d92b0e04c80c4644758dc797d
konovalov07a24ve <vladkonovalov20060@gmail.com>
2025-02-01 22:06:26 +0500

    Добавляет README.md

---
+++ README.md
@@ -0,0 +1,22 @@
+#### Для запуска требуется сам docker
+
+### Самое главное
+Укажите в docker-compose.yml абсолютный путь до проекта
+
+### Шаги для полноценной работы с контейнером
+
+### Шаг 1
+```shell
+make restart
+```
+
+### Шаг 2
+```shell
+docker exec -it allure-container sh
+```
+
+### Шаг 3
+```shell
+cd scripts
+./allure.sh
+```
--- scripts/allure.sh
+++ scripts/allure.sh
@@ -1,16 +1,12 @@
-HOST_IP="0.0.0.0"  # IP внутри контейнера
-HOST_PORT="9090"  # Порт внутри контейнера
-
+HOST_IP="0.0.0.0"
+HOST_PORT="9090"
 TEST_DIR="/app/reports/allure-test"
 REPORT_DIR="/app/reports/allure-report"
 
-# Переходим в рабочую директорию
 cd /app/repo || { echo "Failed to change directory"; exit 1; }
 
-# Находим все директории allure-results по всему проекту
 allure_dirs=$(find . -type d -name "allure-results")
 
-# Копируем все .json файлы в директорию отчётов
 for dir in $allure_dirs; do
   if [ -d "$dir" ]; then
     jsonFiles=$(find "$dir" -type f -name "*.json")
@@ -23,6 +19,5 @@ done
 
 # Генерация отчета с очисткой целевой директории
 allure generate "$TEST_DIR" -o "$REPORT_DIR" --clean
-
 # Открытие отчета
 allure open "$REPORT_DIR" -h "$HOST_IP" -p "$HOST_PORT"
